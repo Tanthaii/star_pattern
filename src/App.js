17b66e88cea1b27e6559cc4a281bc49d021b362d
Tan <hyoka.jy01@gmail.com>
2024-10-23 23:17:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function StarPattern() {
+  const [number, setNumber] = useState(0);
+  const [pattern, setPattern] = useState([]);
+
+  const handleInputChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const generatePattern = () => {
+    const n = parseInt(number);
+    let tempPattern = [];
+
+    
+    for (let i = 1; i <= n; i++) {
+      tempPattern.push('*'.repeat(i));
+    }
+
+   
+    for (let i = n - 1; i >= 1; i--) {
+      tempPattern.push('*'.repeat(i));
+    }
+
+    setPattern(tempPattern);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Star Pattern Generator</h1>
+      <input 
+        type="number" 
+        value={number} 
+        onChange={handleInputChange} 
+        placeholder="Enter a number" 
+      />
+      <button onClick={generatePattern}>Submit</button>
+
+      <div>
+        {pattern.map((line, index) => (
+          <div key={index}>{line}</div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default StarPattern;
